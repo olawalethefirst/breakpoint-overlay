f2b38c591cd7189f8d491f1b2f89540c8a24068a
olawalethefirst <qbashy@gmail.com>
2025-10-31 11:47:54 +0100

    update default shortcut key combo

--- packages/overlay/src/core/defaults.ts
+++ packages/overlay/src/core/defaults.ts
@@ -2,7 +2,7 @@ import type { NormalizedBreakpoint } from './types';
 
 export const CONFIG_SCHEMA_VERSION = 1;
 
-export const DEFAULT_HOTKEY = 'ctrl+shift+b';
+export const DEFAULT_HOTKEY = 'alt+shift+o';
 export const DEFAULT_DEBOUNCE_MS = 150;
 export const DEFAULT_PERSIST_STATE = false;
 
