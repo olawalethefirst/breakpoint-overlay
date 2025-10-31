af8cb13e53d3f95b4d35ac065e30a8c91b06e174
olawalethefirst <qbashy@gmail.com>
2025-10-31 12:37:39 +0100

    remove unsued   DEFAULT_PERSIST_STATE

--- packages/overlay/src/core/defaults.ts
+++ packages/overlay/src/core/defaults.ts
@@ -4,6 +4,5 @@ export const CONFIG_SCHEMA_VERSION = 1;
 
 export const DEFAULT_HOTKEY = 'alt+shift+o';
 export const DEFAULT_DEBOUNCE_MS = 150;
-export const DEFAULT_PERSIST_STATE = false;
 
 export const createDefaultBreakpoints = (): NormalizedBreakpoint[] => [];
